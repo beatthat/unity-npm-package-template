4f381fb49bf9b4fbb71e07da4b63c45f676789f3
larrykirschner <larrykirschner@gmail.com>
2017-09-29 02:30:09 -0700

    fixed cp scripts

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -4,12 +4,15 @@ const path = require('path');
 const unitySrcRoot = path.join(__dirname, '..');
 const tgtPkgRoot = path.join(unitySrcRoot, '..');
 
-unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, (err, info) => {
-    if(err) {
+unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot)
+    .then(info => {
+        const pkgName = (info && info.package) ?
+            info.package.name || '[package name unset]' :
+            '[package info missing from result]';
+
+        console.log(`cp-test2src succeeded for package ${pkgName}`);
+    })
+    .catch(err => {
         console.log('cp-test2src failed with error: %j', err);
         process.exit(1);
-        return;
-    }
-
-    console.log(`cp-test2src succeeded for package ${info.package.name}`);
-});
+    })
--- test/scripts/overwrite-test2src.js
+++ test/scripts/overwrite-test2src.js
@@ -4,12 +4,15 @@ const path = require('path');
 const unitySrcRoot = path.join(__dirname, '..');
 const tgtPkgRoot = path.join(unitySrcRoot, '..');
 
-unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, {overwrite: true}, (err, info) => {
-    if(err) {
-        console.log('overwrite-test2src failed with error: %j', err);
-        process.exit(1);
-        return;
-    }
+unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, { overwrite: true })
+    .then(info => {
+        const pkgName = (info && info.package) ?
+            info.package.name || '[package name unset]' :
+            '[package info missing from result]';
 
-    console.log(`overwrite-test2src succeeded for package ${info.package.name}`);
-});
+        console.log(`cp-test2src succeeded for package ${pkgName}`);
+    })
+    .catch(err => {
+        console.log('cp-test2src failed with error: %j', err);
+        process.exit(1);
+    })
