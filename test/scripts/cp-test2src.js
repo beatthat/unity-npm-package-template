75d67d6003f000e5e4dd47733c5d461d7e795661
larrykirschner <larrykirschner@gmail.com>
2018-03-13 00:31:57 -0700

    call cp-test2src w just package name

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -1,18 +1,16 @@
-const unpm = require('unity-npm-utils');
-const path = require('path');
+const unpm = require('unity-npm-utils')
+const path = require('path')
 
-const unitySrcRoot = path.join(__dirname, '..');
-const tgtPkgRoot = path.join(unitySrcRoot, '..');
+const unitySrcRoot = path.join(__dirname, '..')
+const tgtPkgRoot = path.join(unitySrcRoot, '..')
 
-unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot)
+const pkgName = path.basename(path.dirname(path.join(unitySrcRoot, '..')))
+
+unpm.copyFromUnity2Pkg(pkgName, { verbose: true })
     .then(info => {
-        const pkgName = (info && info.package) ?
-            info.package.name || '[package name unset]' :
-            '[package info missing from result]';
-
-        console.log(`cp-test2src succeeded for package ${pkgName}`);
+        console.log(`cp-test2src succeeded for package ${pkgName}`)
     })
     .catch(err => {
-        console.log('cp-test2src failed with error: %j', err);
-        process.exit(1);
+        console.log('cp-test2src failed with error: %j', err)
+        process.exit(1)
     })
