75693b8097ae1eb56e5c62bd52400c946272df98
larrykirschner <larrykirschner@gmail.com>
2018-05-05 23:17:39 -0700

    updated test script

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -25,8 +25,9 @@ const path = require('path');
 
 const unitySrcRoot = path.join(__dirname, '..');
 const tgtPkgRoot = path.join(unitySrcRoot, '..');
+const pkgName = path.basename(path.join(unitySrcRoot, '..'))
 
-unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, { overwrite: false })
+unpm.copyFromUnity2PkgRoot(pkgName, unitySrcRoot, tgtPkgRoot, { overwrite: false })
     .then(info => {
         const pkgName = (info && info.package) ?
             info.package.name || '[package name unset]' :
@@ -35,6 +36,6 @@ unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, { overwrite: false })
         console.log(`cp-test2src succeeded for package ${pkgName}`);
     })
     .catch(err => {
-        console.log('cp-test2src failed with error: %j', err);
+        console.log('cp-test2src failed with error: %j: %j', err, err.stack);
         process.exit(1);
     })
