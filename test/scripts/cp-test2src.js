95d52a41892fc2dcdc66c1f7db0974ce21290eed
larrykirschner <larrykirschner@gmail.com>
2018-05-05 21:57:34 -0700

    pass explicit path info form cp-test2src

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -4,12 +4,18 @@ const path = require('path')
 const unitySrcRoot = path.join(__dirname, '..')
 
 const pkgName = path.basename(path.join(unitySrcRoot, '..'))
+const unityInstallPath = path.join(__dirname, '..', 'Assets', 'Plugins', 'packages', pkgName)
+const packageTargetPath = path.join(unitySrcRoot, 'Runtime')
 
-unpm.copyFromUnity2Pkg(pkgName, { verbose: true })
+unpm.copyFromUnity2Pkg(pkgName, {
+	verbose: true,
+	unity_install_path: unityInstallPath,
+	package_target_path: packageTargetPath
+})
     .then(info => {
         console.log(`cp-test2src succeeded for package ${pkgName}`)
     })
     .catch(err => {
-        console.log('cp-test2src failed with error: %j', err)
+        console.log('cp-test2src failed with error: %j\n%j', err, err.stack)
         process.exit(1)
     })
