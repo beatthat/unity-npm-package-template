a67a7a0af25096d0c1195cdc0c63dc91293423fb
larrykirschner <larrykirschner@gmail.com>
2018-05-05 17:16:33 -0700

    postinstall changed to call unpm.unityProject.installPackageToUnity as a promise rather than with a callback (prev version was causing unhandled promise-rejection errors)

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,14 +1,21 @@
-const unpm = require('unity-npm-utils');
-const path = require('path');
+const unpm = require('unity-npm-utils')
+const path = require('path')
 
-const pkgRoot = path.join(__dirname, '..');
+const pkgRoot = path.join(__dirname, '..')
 
-unpm.unityProject.installPackageToUnity(pkgRoot, {
-    // verbose: opts.verbose
-},
-(err, info) => {
-    if(err) {
-        console.error('install to unity failed with error: ', err);
-        return;
-    }
-});
+// unpm.unityProject.installPackageToUnity(pkgRoot, {
+//     // verbose: opts.verbose
+// },
+// (err, info) => {
+//     if(err) {
+//         console.error('install to unity failed with error: ', err);
+//         return;
+//     }
+// });
+unpm.unityProject.installPackageToUnity(pkgRoot)
+.then(info => {
+	//console.log('success')
+})
+.catch(e => {
+	console.log(`postinstall failed with error: ${e}`)
+})
