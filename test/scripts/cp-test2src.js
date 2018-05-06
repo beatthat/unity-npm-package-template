593cd2b943dcd274635667e131c99770046cde3f
larrykirschner <larrykirschner@gmail.com>
2018-05-05 22:32:33 -0700

    pass explicit path info form cp-test2src

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -5,7 +5,7 @@ const unitySrcRoot = path.join(__dirname, '..')
 
 const pkgName = path.basename(path.join(unitySrcRoot, '..'))
 const unityInstallPath = path.join(__dirname, '..', 'Assets', 'Plugins', 'packages', pkgName)
-const packageTargetPath = path.join(unitySrcRoot, '..', 'Runtime')
+const packageTargetPath = path.join(unitySrcRoot, '..', 'Runtime', pkgName)
 
 unpm.copyFromUnity2Pkg(pkgName, {
 	verbose: true,
