1caac412f791bce78ba9eb9d77acda07da07754c
larrykirschner <larrykirschner@gmail.com>
2018-05-05 23:27:16 -0700

    updated test copy/overwrite src scripts

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -1,25 +1,3 @@
-// const unpm = require('unity-npm-utils')
-// const path = require('path')
-//
-// const unitySrcRoot = path.join(__dirname, '..')
-//
-// const pkgName = path.basename(path.join(unitySrcRoot, '..'))
-// const unityInstallPath = path.join(__dirname, '..', 'Assets', 'Plugins', 'packages', pkgName)
-// const packageTargetPath = path.join(unitySrcRoot, '..', 'Runtime', pkgName)
-//
-// unpm.copyFromUnity2Pkg(pkgName, {
-// 	verbose: true,
-// 	unity_install_path: unityInstallPath,
-// 	package_target_path: packageTargetPath
-// })
-//     .then(info => {
-//         console.log(`cp-test2src succeeded for package ${pkgName}`)
-//     })
-//     .catch(err => {
-//         console.log('cp-test2src failed with error: %j\n%j', err, err.stack)
-//         process.exit(1)
-//     })
-
 const unpm = require('unity-npm-utils');
 const path = require('path');
 
